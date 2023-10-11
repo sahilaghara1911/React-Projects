85a791668720ea0e26c6c30fa528e9c23760848c
[name] <nitinpatel9860@gmail.com>
2023-10-11 22:42:20 +0530

    Done

--- Trip-Plan-App/src/App.jsx
+++ Trip-Plan-App/src/App.jsx
@@ -4,11 +4,26 @@ import Tours from "./components/Tours";
 
 import "./App.css";
 
-function App() {
+const App = () =>{
   const [tours, setTours] = useState(data);
+  function removeTour(id){
+        const newTours = tours.filter(tour => tour.id !== id);
+        setTours(newTours)
+  }
+
+  if(tours.length ===0 ){
+    return (
+      <div className="refresh flex justify-center items-center flex-col h-[100vh]">
+        <h2 className="font-bold text-lg">No Tours Left</h2>
+        <button onClick={() => setTours(data)} className="border mt-[18px] py-[10px] px-[80px] bg-slate-500">
+          Refresh
+        </button>
+      </div>
+    )
+  }
   return (
-    <div>
-      <Tours tours={tours}></Tours>
+    <div className="min-h-[100vh]">
+      <Tours tours={tours} removeTour={removeTour}></Tours>
     </div>
   );
 }
