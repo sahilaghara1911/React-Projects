aa0c72de6cfe385e8f58c04bfec422798e5c0d3d
[name] <nitinpatel9860@gmail.com>
2023-10-11 22:42:33 +0530

    Done

--- Trip-Plan-App/src/components/Tours.jsx
+++ Trip-Plan-App/src/components/Tours.jsx
@@ -1,12 +1,13 @@
-function Tours({ tours }) {
+import Card from "./Card";
+function Tours({ tours , removeTour}) {
   return (
-    <div>
+    <div className="flex justify-center items-center flex-col">
       <div>
-        <h2>Plan with Love</h2>
+        <h2 className="text-[3rem] m-[6vh] border-dotted border-cyan-400 border-8 rounded-lg py-[1vh] px-[5vw] font-bold ">Trip Plan</h2>
       </div>
-      <div>
+      <div className="flex items-center justify-center flex-wrap max-w-[1300px] mx-auto">
         {tours.map((tours) => {
-          return <Card {...tours}></Card>;
+          return <Card {...tours} removeTour={removeTour}></Card>;
         })}
       </div>
     </div>
