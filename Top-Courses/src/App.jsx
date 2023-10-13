f555bc67ebb79ae4161e831d7efa2de2432379f3
[name] <nitinpatel9860@gmail.com>
2023-10-13 22:58:35 +0530

    done

--- Top-Courses/src/App.jsx
+++ Top-Courses/src/App.jsx
@@ -2,28 +2,48 @@ import { useEffect, useState } from "react"
 import { apiUrl, filterData } from "./data"
 import Navbar from "./Components/Navbar";
 import Filter from "./Components/Filter";
+import Cards from "./Components/Cards";
+import Spinner from "./Components/Spinner";
 
-function App() {
 
-  useEffect( () => {
-    const fetchData = async() => {
-      try{
-        
+    function App() {
+      const [courses, setCourses] = useState(null);
+      const [loading, setLoading] = useState(true);
+
+      async function fetchData(){
+        setLoading(true);
+        try{
+          let res = await fetch(apiUrl);
+          let output = await res.json()
+          // Output->
+          setCourses(output.data)
+        } catch(e){
+          toast.error('Kis line pe hai app')
+        }
+        setLoading(false)
       }
+
+      useEffect( ()=>{
+        fetchData();
+      },[])
+      return (
+          <div>
+            <div>
+              <Navbar />
+              </div>
+                <div>
+                <Filter 
+                filterData={filterData}
+                />
+                </div>
+                <div>
+                {
+                  loading ? (<Spinner />) : (<Cards courses={courses}/>)
+                }
+              
+              </div>
+          </div>
+      )
     }
-  })
-  return (
-    <div>
-      <Navbar />
 
-      <Filter 
-      filterData={filterData}
-
-      />
-
-      
-    </div>
-  )
-}
-
-export default App
+  export default App
