bc19ae04cea7727de42b952ce4bf65d16ebf2e35
[name] <nitinpatel9860@gmail.com>
2023-10-18 11:14:10 +0530

    add css

--- Testimonial-Slider/src/App.jsx
+++ Testimonial-Slider/src/App.jsx
@@ -4,7 +4,7 @@ import Testimonials from './components/Testimonials'
 
 const App = () => {
   return (
-    <div className='flex flex-col w-[100vh] v-[100vh] justify-center items-center bg-gray-200'>
+    <div className='flex flex-col w-[100wh] h-[100vh] justify-center items-center bg-gray-200'>
       <div className='text-center'>
         <h1 className='text-4xl font-bold'>Our Testimonials</h1>
         <div className='bg-violet-400 h-[4px] w-[1/5] mt-1'></div>
