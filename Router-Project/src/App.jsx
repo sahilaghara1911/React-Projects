933fa62055be2e9dda2b9e9e9e0dbd64eb8e9c04
[name] <nitinpatel9860@gmail.com>
2023-10-23 13:56:53 +0530

    Made

---
+++ Router-Project/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <div>
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
