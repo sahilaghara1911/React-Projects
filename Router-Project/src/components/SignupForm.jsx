9b568637a4c20ee71ff326bd82c704452aa96754
[name] <nitinpatel9860@gmail.com>
2023-10-25 21:44:32 +0530

    Add signup page

--- Router-Project/src/components/SignupForm.jsx
+++ Router-Project/src/components/SignupForm.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignupForm = () => {
+  const [formData, setFormData] = useState({
+    firstname:"",
+    lastname:"",
+    email:"",
+    password:"",
+    confirmpassword:""
+  })
   return (
-    <div>SignupForm</div>
+    <div>
+      {/* Student -Instuctor tab */}
+      <div>
+        <button>Student</button>
+        <button>Instuctor</button>
+      </div>
+
+      <form>
+      <div>
+      {/* First anem or last name */}
+        <label>
+            <p>
+              First Name<sup>*</sup>
+            </p>
+            <input type='text' required name='firstname' onChange={chnageHandler}
+            placeholder='Enter first name'
+            value={formData.firstname} />
+        </label>
+
+        <label>
+            <p>
+              Last Name<sup>*</sup>
+            </p>
+            <input type='text' required name='lastname' onChange={chnageHandler}
+            placeholder='Enter last name'
+            value={formData.lastname} />
+        </label>
+      </div>
+      </form>
+    </div>
   )
 }
 
