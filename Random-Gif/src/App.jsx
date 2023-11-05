6301982c7ebff71ce5495c0f0b7d41046b4cd46a
[name] <nitinpatel9860@gmail.com>
2023-11-05 20:10:23 +0530

    style

--- Random-Gif/src/App.jsx
+++ Random-Gif/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className='w-full flex flex-col h-screen background relative items-center'>
       <Heading />
-      <div className='flex flex-col w-full items-center mt-7 gap-y-10'>
+      <div className='flex flex-col w-full items-center mt-7 gap-y-10 '>
         <Random />
         <Tag />
       </div>
