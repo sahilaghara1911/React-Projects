e6dc71d651c5142b2158454313e6771380a1438f
[name] <nitinpatel9860@gmail.com>
2023-11-26 20:28:18 +0530

    Add url

---
+++ Blog-Context/src/context/AppContext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useState } from "react";
+import { baseUrl } from "../baseUrl";
+//step-1
+export const AppContext = createContext();
+
+export default function AppContextProvider({children}) {
+    const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(null);
+
+    //data filling pending
+    async function fetchBlogPosts(page = 1){
+        setLoading(true);
+        let url = `${baseUrl}?page=${page}`
+
+        try{
+            const result = await fetch(url);
+            const data = await result.json();
+            console.log(data);
+            setPage(data.page);
+            setPosts(data.posts);
+            setTotalPages(data.totalPages)
+        } catch(error) {
+            alert("Error hai bhaii");
+            setPage(1);
+            setPosts([]);
+            setTotalPages(null)
+        }
+        setLoading(false)
+    }
+
+    const value = {
+        loading,
+        setLoading,
+        posts,
+        setPage,
+        setPosts,
+        page,
+        totalPages,
+        setTotalPages
+    };
+//step-2
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
