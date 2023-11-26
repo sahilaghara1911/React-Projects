7b5e03346294faf016e09c4ad4bc9244e3cde416
[name] <nitinpatel9860@gmail.com>
2023-11-26 14:09:58 +0530

    setup

---
+++ Blog-Context/src/context/AppContext.js
@@ -0,0 +1,10 @@
+import { createContext, useState } from "react";
+
+export const AppContext = createContext();
+
+export default function AppContextProvider({children}) {
+    const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(null)
+}
